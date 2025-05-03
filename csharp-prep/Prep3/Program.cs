6a867d336783ba57b7bcd7f729c5af89f283a0e9
wicksand <wicksand@byui.edu>
2025-05-03 17:28:05 -0600

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // generate random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        bool running = true;
+
+        // game loop
+        while (running == true){
+            Console.Write("What is your guess? ");
+        string roughGuess = Console.ReadLine();
+        int guess = int.Parse(roughGuess);
+
+        //core logic responces
+        if (number < guess){
+            Console.WriteLine("Lower");
+        }
+        else if(number > guess){
+            Console.WriteLine("Higher");
+        }
+        else{
+            Console.WriteLine("You guessed it!");
+            running = false;
+        }
+        }
+
+        // //initial prompt
+        // Console.Write("What is the magic number? ");
+        // string input = Console.ReadLine();
+        // int numbeer = int.Parse(input);
+        
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,43 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
+    static double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+
+    static void DisplayGreeting()
+    {
+        Console.WriteLine("hi tim");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.Write("Please input your name:");
-        string firstName = Console.ReadLine();
-        Console.WriteLine($"Your first name is: {firstName}");
+        // Console.WriteLine("Hello Sandbox World!");
+        // Console.Write("Please input your name:");
+        // string firstName = Console.ReadLine();
+        // Console.WriteLine($"Your first name is: {firstName}");
+
+        // Random randomGenerator = new Random();
+        // int randomNumber = new RandomNumberGenerator(1,100);
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(50);
+        // myNumbers.Add(51);
+        // myNumbers.Add(52);
+
+        // foreach(int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        double total = AddNumbers(50,45);
+        Console.WriteLine($"The total is:{total}");
+
+        // bool done = false;
+        // do{
+        //     Console.WriteLine("");
+        // }while(!done);
 
 
     }
