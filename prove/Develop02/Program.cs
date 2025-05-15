8e9328db4263e9e346a7686dd0131b3ed3943c4d
wicksand <wicksand@byui.edu>
2025-05-15 10:11:31 -0600

    menu for progect 2

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
+    static int DisplayMenu()
+    {
+        int responce = 0;
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("ParallelLoopState select one of the following choicde");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.Write("What Would you like to do? ");
+        responce = int.Parse(Console.ReadLine());
+        return responce;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int input = 0;
+
+        while (input != 5)
+        {
+            input = DisplayMenu();
+            Console.WriteLine(input);
+            
+        }
+
     }
 }
