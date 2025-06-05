6e24672bf313374ba5c0a8dec70c6f4d6d57b329
wicksand <wicksand@byui.edu>
2025-06-05 09:31:16 -0600

    class inheritance explination.

---
+++ Person/doctor.cs
@@ -0,0 +1,15 @@
+class Doctor : Person
+{
+    private string _tools;
+    public Doctor(string firstName, string lastName, int age, string tools)
+    : base(firstName, lastName, age)
+    {
+
+        _tools = tools;
+    }
+
+    public string GetDoctorInformation()
+    {
+        return $"tools:{_tools} :: {GetPersonInformation()}";
+    }
+}
