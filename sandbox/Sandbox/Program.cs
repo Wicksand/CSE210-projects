8a90e9f3edf0b4990e135e2315985f1252194d1f
wicksand <wicksand@byui.edu>
2025-06-10 10:10:58 -0600

    test timer code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,25 +2,57 @@ using System;
 using System.Diagnostics.Tracing;
 using System.IO.Compression;
 
-
 class Program
 {
     static void Main(string[] args)
     {
-        Circle myCircle = new Circle();
-        // myCircle.SetRadius(10);
-        // Console.WriteLine($"{myCircle.GetRadius()}");
-        // Circle myCircle2 = new Circle();
-        // myCircle2.SetRadius(20);
-        Console.WriteLine($"{myCircle2.GetRadius()}");
+        Console.WriteLine("yo bob");
 
-        Console.WriteLine($"{myCircle.GetArea()}");
-        Console.WriteLine($"{myCircle2.GetArea()}");
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(20);
 
-        Cylinder myCylinder = new Cylinder();
-        myCylinder.SetHeight(10);
-        myCylinder.SetCircle(myCircle);
-        Console.WriteLine($"{myCylinder.GetVolume()}");
+        string animation = ":D:(";
+        string animationString2 = "|/-|-\\";
+        int index = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(animationString2[index++ % animationString2.Length]);
+            Thread.Sleep(500);
+            Console.Write("\b");
+        }
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(animation[0..2]);
+            Thread.Sleep(500);
+            Console.Write("\b\b");
+            Console.Write(animation[2..]);
+            Thread.Sleep(500);
+            Console.Write("\b\b");
+            
+        
+        }
     }
 }
- 
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Circle myCircle = new Circle();
+//         // myCircle.SetRadius(10);
+//         // Console.WriteLine($"{myCircle.GetRadius()}");
+//         // Circle myCircle2 = new Circle();
+//         // myCircle2.SetRadius(20);
+//         Console.WriteLine($"{myCircle2.GetRadius()}");
+
+//         Console.WriteLine($"{myCircle.GetArea()}");
+//         Console.WriteLine($"{myCircle2.GetArea()}");
+
+//         Cylinder myCylinder = new Cylinder();
+//         myCylinder.SetHeight(10);
+//         myCylinder.SetCircle(myCircle);
+//         Console.WriteLine($"{myCylinder.GetVolume()}");
+//     }
+// }
