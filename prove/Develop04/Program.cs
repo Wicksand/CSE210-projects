cc870de51640a1343a87e00e7d6adb08ccaa385f
wicksand <wicksand@byui.edu>
2025-06-12 10:13:51 -0600

    start to relaxation program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int running = 0;
+        while (running != 4){
+            running = DisplayMenu();
+        }
+    }
+    private static int DisplayMenu()
+    {
+        Console.WriteLine("Pick your activity.");
+        // string read = Console.ReadLine();
+        // if (read <){
+
+        // }
+        int input = int.Parse(Console.ReadLine());
+        switch (input)
+        {
+            case 1:
+                Console.WriteLine("case1");
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+            case 4:
+            
+                break;
+            default:
+
+                break;
+        }
+        return input;
     }
 }
