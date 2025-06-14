e8e70548da4f3a6813a1cbc7618a0fad748e8420
wicksand <wicksand@byui.edu>
2025-06-14 16:27:28 -0600

    finnished :)

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,24 +1,25 @@
 class Listing : Activity
 {
 
-    private int _count;
+    private int _count = 0;
     public Listing() : base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
     {
         DisplayWelcome();
         SetTime();
         GetReady();
         DisplayRandomQuestion();
+        RunListing();
         DisplayGoodbye();
     }
     private void DisplayRandomQuestion()
     {
         List<string> questions = new List<string>();
-        questions.Add("---Who are people that you appreciate?---");
-        questions.Add("---What are personal strengths of yours?---");
-        questions.Add("---Who are people that you have helped this week?---");
-        questions.Add("---What was the strongest emotion I felt today?---");
-        questions.Add("---When have you felt the Holy Ghost this month?---");
-        questions.Add("---Who are some of your personal heroes?---");
+        questions.Add("--- Who are people that you appreciate? ---");
+        questions.Add("--- What are personal strengths of yours? ---");
+        questions.Add("--- Who are people that you have helped this week? ---");
+        questions.Add("--- What was the strongest emotion I felt today? ---");
+        questions.Add("--- When have you felt the Holy Ghost this month? ---");
+        questions.Add("--- Who are some of your personal heroes? ---");
 
         Random rand = new Random();
         int _randNum = rand.Next(questions.Count);
@@ -26,6 +27,18 @@ class Listing : Activity
     }
     private void RunListing()
     {
-        
+        Console.Write("You may begin in:");
+        Timer(3);
+        Console.WriteLine();
+
+        DateTime endTime = DateTime.Now.AddSeconds(GetTime());
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(">");
+            Console.ReadLine();
+            _count++;
+        }
+        Console.WriteLine($"You listed {_count} Items!");
     }
 }
--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -1,10 +1,56 @@
+using System.Reflection;
+
 class Reflection : Activity
 {
-    public Reflection() : base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    public Reflection() : base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. \nThis will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
         DisplayWelcome();
         SetTime();
-        GetReady();
+        DisplayRandomPrompt();
+        Console.WriteLine("\nWhen you have something in mind, press enter to continue.");
+        Console.ReadLine();
+        RunReflection();
         DisplayGoodbye();
+    }
+    private void DisplayRandomPrompt()
+    {
+         List<string> prompts = new List<string>();
+        prompts.Add("--- Think of a time when you stood up for someone else. ---");
+        prompts.Add("--- Think of a time when you did something really difficult. ---");
+        prompts.Add("--- Think of a time when you helped someone in need. ---");
+        prompts.Add("--- Think of a time when you did something truly selfless. ---");
+        
+        Random rand = new Random();
+        int _randNum = rand.Next(prompts.Count);
+        Console.WriteLine(prompts[_randNum]);
+    }
+    private void DisplayRandomFollowQuestion()
+    {
+         List<string> followQuestions = new List<string>();
+        followQuestions.Add("Why was this experience meaningful to you?");
+        followQuestions.Add("Have you ever done anything like this before?");
+        followQuestions.Add("How did you get started?");
+        followQuestions.Add("What made this time different than other times when you were not as successful?");
+        followQuestions.Add("What is your favorite thing about this experience?");
+        followQuestions.Add("What could you learn from this experience that applies to other situations?");
+        followQuestions.Add("What did you learn about yourself through this experience?");
+        followQuestions.Add("How can you keep this experience in mind in the future?");
+
+        Random rand = new Random();
+        int _randNum = rand.Next(followQuestions.Count);
+        Console.WriteLine(followQuestions[_randNum]);
+    }
+    private void RunReflection()
+    {
+        Console.WriteLine("Now ponder on each of the following questionsas they related to this experience.");
+        Console.Write("You may begin in:");
+        Timer(5);
+        DateTime endTime = DateTime.Now.AddSeconds(GetTime());
+        Console.Clear();
+        while (DateTime.Now < endTime)
+        {
+            DisplayRandomFollowQuestion();
+            Spinner(5);
+        }
     }
 }
