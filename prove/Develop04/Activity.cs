b2c16846c9098343a74d16416e30fc7790f01ff8
wicksand <wicksand@byui.edu>
2025-06-14 13:19:43 -0600

    activity time request

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Data;
 
 class Activity
 {
@@ -16,9 +17,21 @@ class Activity
         _description = description;
 
     }
+    public void SetTime()
+    {
+        int timeLen;
+        
+        Console.WriteLine("How long, in seconds, would you like for your session?");
+
+        while (!int.TryParse(Console.ReadLine(), out timeLen))//keeps checking until valid input
+        {
+            Console.WriteLine("\n--------------------------------------------------------------------\nInvalid input, Please enter a number of seconds\n--------------------------------------------------------------------\n");
+        }
+        _timeLen = timeLen;
+    }
 
     protected void DisplayWelcome()
     {
-        Console.WriteLine($"Welcome to the {_name} Activity");
+        Console.WriteLine($"Welcome to the {_name} Activity\n\n{_description}");
     }
 }
--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -4,6 +4,7 @@ class Breathing : Activity
     public Breathing() : base("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
     {
         DisplayWelcome();
+        SetTime();
     }
     public void RunBreath()
     {
