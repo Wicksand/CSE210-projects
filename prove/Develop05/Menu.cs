4cef6792db034c8812d687a4203228aa23a49dfc
wicksand <wicksand@byui.edu>
2025-07-02 23:49:24 -0600

    created displayGoalMenue

--- prove/Develop05/Menu.cs
+++ prove/Develop05/Menu.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 using System.Reflection;
 
 class Menu
@@ -15,8 +16,9 @@ class Menu
     }
     private int DisplayMenu()
     {
+        Console.Clear();
         Console.WriteLine($"You have {_score} points\n");
-        Console.WriteLine("Menu Options:\n 1. Create New Goal\n 2. List Goals\n 3. Save Goals\n 4. Save Goals\n 5. Record Event\n 6. Quit");
+        Console.WriteLine("Menu Options:\n  1.  Create New Goal\n  2.  List Goals\n  3.  Save Goals\n  4.  Save Goals\n  5.  Record Event\n  6.  Quit");
         Console.Write("Select a choice from the menu: ");
 
 
@@ -31,7 +33,7 @@ class Menu
         switch (menuVal)
         {
             case 1:
-                Console.WriteLine("It worked");
+                DisplayGoalMenu();
                 break;
             case 2:
                 Console.WriteLine("It worked");
@@ -48,7 +50,6 @@ class Menu
 
                 break;
             case 6:
-                Console.WriteLine("It worked");
 
                 break;
             default:
@@ -56,8 +57,21 @@ class Menu
                 Console.WriteLine("\n--------------------------------------------------------------------\nInvalid input, Please enter a number 1-6\n--------------------------------------------------------------------\n");
                 break;
         }
-         return menuVal;
+        return menuVal;
     }
 
+    private void DisplayGoalMenu()
+    {
+        // string read = Console.ReadLine();
+        // if (!int.TryParse(read, out int goalType))
+        // {
+        //     Console.Clear();
+        //     Console.WriteLine("\n--------------------------------------------------------------------\nInvalid input, Please enter a number 1-3\n--------------------------------------------------------------------\n");
+        //     return 0;
+        // }
+        Console.WriteLine("The types of Goals are:\n  1.  Simple Goal\n  2.  Eternal Goal\n  3.  Checklist Goal");
+        Console.Write("Which type of goal would you like to create? ");
+        Console.ReadLine();
+    } 
 
 }
