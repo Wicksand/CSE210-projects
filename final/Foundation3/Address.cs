9f8c79b2a04d15f77225c9268f6f6e2342594d5b
wicksand <wicksand@byui.edu>
2025-07-12 22:59:35 -0600

    outline project 3

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+
+class ReceptionEvent
+{
+    public ReceptionEvent()
+    {
+        
+    }
+}
---
+++ final/Foundation3/LectureEvent.cs
@@ -0,0 +1,7 @@
+class LectureEvent
+{
+    public LectureEvent()
+    {
+        
+    }
+}
